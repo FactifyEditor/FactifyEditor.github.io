5ff0006734d86bded64cba3924ba92e5dee854af
Utkarsh Shukla <utk.shukla@gmail.com>
2023-03-17 19:00:44 +0530

    Changed Config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://factifyeditor.github.ios',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/FactifyEditor.github.io/',
+  baseUrl: '/build/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
